14b4a5efe406a32fc27f4711028e1d8ed5b9ddb7
Tamir Banay <banay9329@gmail.com>
2024-01-28 13:42:52 +0200

    add register module

--- Api.js
+++ Api.js
@@ -4,6 +4,7 @@ const port = 3000;
 
 // Store logs in memory for this example
 const logs = [];
+let connectedModules = {};
 
 app.use(express.json());
 
@@ -19,13 +20,48 @@ app.post("/api/logs", (req, res) => {
   }
 });
 
-// Serve an HTML page with the logs
-app.get('/logs', (req, res) => {
+app.post("/api/register", (req, res) => {
+  const { id, details } = req.body;
+  connectedModules[id] = { ...details, lastSeen: new Date() };
+  res.status(200).send("Module registered");
+});
+
+app.get("/api/modules", (req, res) => {
+  res.status(200).json(connectedModules);
+});
+
+app.get("/", (req, res) => {
+  let html = `
+    <!DOCTYPE html>
+    <html lang='en'>
+    <head>
+        <title>Module Details</title>
+    </head>
+    <body>
+        <button id="fetchModules">Get Connected Modules</button>
+        <div id="modulesInfo"></div>
+
+        <script>
+            document.getElementById('fetchModules').onclick = function() {
+                fetch('/api/modules')
+                    .then(response => response.json())
+                    .then(data => {
+                        document.getElementById('modulesInfo').innerHTML = JSON.stringify(data, null, 2);
+                    });
+            };
+        </script>
+    </body>
+    </html>
+    `;
+  res.send(html);
+});
+
+app.get("/logs", (req, res) => {
   let html = `<html><head><title>Logs</title></head><body>`;
   html += `<h1>Logs</h1>`;
   html += `<ul>`;
   // Add each log to the HTML as a list item
-  logs.forEach(log => {
+  logs.forEach((log) => {
     html += `<li>${JSON.stringify(log)}</li>`;
   });
   html += `</ul>`;
