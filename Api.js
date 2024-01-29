e4fbdaa2fe345c940f626ee70f9068ddf36e3b1d
Tamir Banay <banay9329@gmail.com>
2024-01-29 19:52:51 +0200

    v

--- Api.js
+++ Api.js
@@ -260,6 +260,6 @@ app.post("/api/test", (req, res) => {
     });
 });
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server listening on port ${port}`);
 });
