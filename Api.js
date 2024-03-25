6e7a3c478b5f75d43d0b7d37db59ca499bc80148
Tamir Banay <banay9329@gmail.com>
2024-03-25 15:33:38 +0200

    add fs

--- Api.js
+++ Api.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const moduleModel = require("./mongoDB/Modules");
 const logsModel = require("./mongoDB/Logs");
 const citiesFilePath = "./cities.json";
+const fs = require("fs");
+
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
